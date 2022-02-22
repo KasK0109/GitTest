f4800a8cbedef41c50f33bc6828b9dc3e736c8cf
Kaare Skau <kaare3014@gmail.com>
2022-02-22 10:05:44 +0100

    Initial commit

---
+++ Test/src/gittest/Main.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Person p1 = new Person("Hans", 30);
+        Person p2 = new Person("Ulla", 31);
+
+        System.out.println("Navn: " + p1.getName() + " Alder: " + p1.getAge());
+        System.out.println("Navn: " + p2.getName() + " Alder: " + p2.getAge());
+    }
+}
